29a9bc39854b4380bd3641cb36cc293b8d2db1da
Leonardo Spairani <leospairani@gmail.com>
2024-01-16 10:31:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Example Resources
+
+https://fastapi.blog/blog/posts/2023-07-20-fastapi-sqlalchemy-migrations-guide/#fastapi-sqlalchemy-migrations-guide
+
---
+++ storeapi/database.py
@@ -0,0 +1,44 @@
+import databases
+import sqlalchemy
+
+from storeapi.config import config
+
+metadata = sqlalchemy.MetaData()
+
+post_table = sqlalchemy.Table(
+    "posts",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("body", sqlalchemy.String)
+)
+
+comment_table = sqlalchemy.Table(
+    "comments",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("body", sqlalchemy.String),
+    sqlalchemy.Column("post_id", sqlalchemy.ForeignKey("posts.id"), nullable=False)
+)
+
+test_table = sqlalchemy.Table(
+    "tests_migrations",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("test", sqlalchemy.String)
+)
+
+test_table_2 = sqlalchemy.Table(
+    "tests_migrations_2",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("test", sqlalchemy.String)
+)
+
+engine = sqlalchemy.create_engine(
+    config.DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+metadata.create_all(engine)
+database = databases.Database(
+    config.DATABASE_URL, force_rollback=config.DB_FORCE_ROLL_BACK
+)
