dc80aa69857970c32828277c93defd9ce9cc18d2
Leonardo Spairani <leospairani@gmail.com>
2024-01-17 09:42:34 +0100

    Removed database creation in favor of alembic

--- storeapi/database.py
+++ storeapi/database.py
@@ -38,7 +38,6 @@ engine = sqlalchemy.create_engine(
     config.DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-metadata.create_all(engine)
 database = databases.Database(
     config.DATABASE_URL, force_rollback=config.DB_FORCE_ROLL_BACK
 )
